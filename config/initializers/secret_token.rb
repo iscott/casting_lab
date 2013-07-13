3558092cc12b7f3f7dad199b0a6eb9c8de99f379
Ira Herman <ira@xynetik.com>
2013-07-13 16:23:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hhd::Application.config.secret_token = 'aa206301aca53db544e2be4d87731aee4141a1155cbf1c7ff5ae01d795dcd91e6a1e68e4e322dc33b5d4b7f4d9c8266b5afa1a0b91b50bc8ce645c279c735be2'
